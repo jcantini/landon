b14d9fe05f5df5342303bc004571d1afc0a46315
Jorge Cantini <jorge.cantinigmail.com>
2023-07-06 14:59:51 -0300

    2nd commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in less than a day
+          React App deployed in AWS
         </a>
       </header>
     </div>
